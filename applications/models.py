c50396d22f365725288b3bd6e168ab8f44454325
Leakey <otienoleakey287@gmail.com>
2026-02-18 14:15:57 +0300

    Added job and applications features

--- applications/models.py
+++ applications/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.conf import settings
+from jobs.models import Job
 
-# Create your models here.
+class Application(models.Model):
+    STATUS_CHOICES = (
+        ("pending", "Pending"),
+        ("hired", "Hired"),
+        ("completed", "Completed"),
+        ("rejected", "Rejected"),
+    )
+
+    job = models.ForeignKey(Job, on_delete=models.CASCADE)
+    youth = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="pending")
+    applied_at = models.DateTimeField(auto_now_add=True)
--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Skill(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+class Job(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    sub_county = models.CharField(max_length=100)
+    vacancies = models.IntegerField()
+    is_open = models.BooleanField(default=True)
+    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    required_skills = models.ManyToManyField(Skill)
+    created_at = models.DateTimeField(auto_now_add=True)
